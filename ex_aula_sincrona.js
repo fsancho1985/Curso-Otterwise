3b3cd1da9bc13f22c4a0d4b4b424067d85e91fd7
Francisco <frsancho1985@gmail.com>
2021-08-19 18:41:44 -0300

    correcao exercicio aula sincrona

--- ex_aula_sincrona.js
+++ ex_aula_sincrona.js
@@ -1,23 +1,34 @@
-let nome = 'Francisco'
-let sobrenome = 'Sancho'
-let diaNasc = 23
-let mesNasc = 11
-let anoNasc = 1985
-let mail = 'frsancho1985@gmail.com'
-let ddd = '(71)'
-let tel = '99130-9384'
-let namoro = false
-let casamento = false
+// let nome = 'Francisco'
+// let sobrenome = 'Sancho'
+// let diaNasc = 23
+// let mesNasc = 11
+// let anoNasc = 1985
+// let mail = 'frsancho1985@gmail.com'
+// let ddd = '71'
+// let tel = '99130-9384'
+// let namoro = false
+// let casamento = false
 
-const nomeCompleto = nome + sobrenome
-const idade = 35
-const nascimento = diaNasc + '/' + mesNasc + '/' + anoNasc
-const email = mail
-const telefone = ddd + '' + tel
+const nome = 'Francisco'
+const sobrenome = 'Sancho'
+const dia = 23
+const mes = 11
+const ano = 1985
+const email = 'frsancho1985@gmail.com'
+const ddd = '71'
+const telefone = '991309384'
+const namoro = false
+const casamento = false
 
-console.log('Nome completo: ' + nomeCompleto)
-console.log('Idade: ' + idade)
-console.log('Data de Nascimento: ' + nascimento)
+// const nomeCompleto = nome + sobrenome
+// const idade = 35
+// const nascimento = diaNasc + '/' + mesNasc + '/' + anoNasc
+// const email = mail
+// const telefone = ddd + '' + tel
+
+console.log('Nome completo: ' + nome + ' ' + sobrenome)
+console.log('Idade: ' + (2021 - ano))
+console.log('Data de Nascimento: ' + dia + '/' + mes + '/' + ano)
 console.log('E-mail: ' + email)
-console.log('Telefone: ' + telefone)
-console.log('Está em um relacionamento? ' + (namoro && casamento))
+console.log('Telefone: (' + ddd + ')' + telefone)
+console.log('Está em um relacionamento? ' + (namoro || casamento))
