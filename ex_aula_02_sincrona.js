d591a00fb4e263c1372f7494a0e07522ca4d65c0
Francisco <frsancho1985@gmail.com>
2021-08-23 19:09:43 -0300

    Correcao exercicios

--- ex_aula_02_sincrona.js
+++ ex_aula_02_sincrona.js
@@ -10,40 +10,48 @@ const nota3 = 6
 function media(a, b, c) {
     return (nota1 + nota2 + nota3)/3
 }
+const mediaAluno = media(nota1, nota2, nota3).toFixed(2)
 console.log(media(nota1, nota2, nota3).toFixed(2))
 
 // função para verificar aprovação do estudante
-function checkGraduate(x) {
-    if (x < 4){
-        return 'Reprovado'
-    } else if (x >= 6) {
+function checkGraduate(mediaAluno) {
+    if (mediaAluno >= 6){
         return 'Aprovado'
-    } else if (x >= 4) {
-        return 'Em exame'
+    } else if (mediaAluno >= 4) {
+        return 'Exame'
+    } else {
+        return 'Reprovado'
     }
 }
 
 console.log(checkGraduate(media(nota1, nota2, nota3)))
 
 // função que informa se o aluno está ou não aprovado
-function inform(x) {
-    if (idade < 18) {
-        if (checkGraduate === 'Aprovado') {
-            return responsavel + 'O aluno ' + nome + '' + sobrenome + ' está APROVADO'
-        } else if (checkGraduate === 'Reprovado') {
-            return responsavel + ', o aluno ' + nome + '' + sobrenome + ' está REPROVADO'
-        } else if (checkGraduate === 'Em exame') {
-            return responsavel + ', o aluno ' + nome + '' + sobrenome + ' está EM EXAME'
-        }
+function inform(nome, responsavel, idade, mediaAluno) {
+    if (idade >= 18) {
+        console.log(nome + ' você está em ' + checkGraduate(mediaAluno))
     } else {
-        if (checkGraduate === 'Aprovado') {
-            return nome + '' + sobrenome + ' está APROVADO'
-        } else if (checkGraduate === 'Reprovado') {
-            return nome + '' + sobrenome + ' está REPROVADO'
-        } else if (checkGraduate === 'Em exame') {
-            return nome + '' + sobrenome + ' está EM EXAME'
-        }
+        console.log(responsavel + ', ' + nome + ' está em ' + checkGraduate(mediaAluno))
     }
 }
 
-console.log(inform(checkGraduate()))
+inform(nome, responsavel, idade, mediaAluno)
+// function inform(x) {
+//     if (idade < 18) {
+//         if (checkGraduate === 'Aprovado') {
+//             return responsavel + 'O aluno ' + nome + '' + sobrenome + ' está APROVADO'
+//         } else if (checkGraduate === 'Reprovado') {
+//             return responsavel + ', o aluno ' + nome + '' + sobrenome + ' está REPROVADO'
+//         } else if (checkGraduate === 'Em exame') {
+//             return responsavel + ', o aluno ' + nome + '' + sobrenome + ' está EM EXAME'
+//         }
+//     } else {
+//         if (checkGraduate === 'Aprovado') {
+//             return nome + '' + sobrenome + ' está APROVADO'
+//         } else if (checkGraduate === 'Reprovado') {
+//             return nome + '' + sobrenome + ' está REPROVADO'
+//         } else if (checkGraduate === 'Em exame') {
+//             return nome + '' + sobrenome + ' está EM EXAME'
+//         }
+//     }
+// }
