264df856d65b960f9a46c155394cd99b85f4279a
Francisco <frsancho1985@gmail.com>
2021-10-03 14:53:19 -0300

    Refatorando código

--- desafio/desafio_final_js_avancado/main.js
+++ desafio/desafio_final_js_avancado/main.js
@@ -1,30 +1,9 @@
 // Importando as funções
+import { buscaCliente } from './formatters/formatters.js'
 
-import { filtraCliente, filtraCompra, dateChange, order, desorder } from './formatters/formatters.js'
 
-// Importando função reduce
-
-import { sum, show } from './math/math.js'
-
-// Importando const clientes
-// import { clientes } from './formatters/formatters.js'
-
-const newClientes = filtraCliente(1)
-
-const newCompras = filtraCompra(1)
-
-const comprasOrdered = desorder(order(dateChange(newCompras)))
-
-/*----------------------------------------------------------------------------------------------------------*/
-
-const newClientes2 = filtraCliente(2)
-
-const newCompras2 = filtraCompra(2)
-
-const comprasOrdered2 = desorder(order(dateChange(newCompras2)))
-
-const totalCliente1 = sum(comprasOrdered)
-
-const totalCliente2 = sum(comprasOrdered2)
-
-show(newClientes, comprasOrdered, totalCliente1, newClientes2, comprasOrdered2, totalCliente2)
+buscaCliente(1)
+buscaCliente(2)
+buscaCliente(3)
+buscaCliente(5)
+buscaCliente(10)
--- desafio/desafio_final_js_avancado/math/math.js
+++ desafio/desafio_final_js_avancado/math/math.js
@@ -1,19 +1,19 @@
 // Importando as funções
-import { filtraCliente, filtraCompra, dateChange, order, desorder } from '../formatters/formatters.js'
+// import { filtraCliente, filtraCompra, dateChange, order, desorder } from '../formatters/formatters.js'
 
 export function sum(p) {
     const total = p.reduce((acc, elem) => acc + elem.total, 0)
     return total
 }
 
-export function show(p, q, r, s, t, u) {
-    console.log(`Cliente: ${p.id} - ${p.name}`)
-    console.log(`Compra em ${q[0].data} no total de R$${q[0].total.toFixed(2)}`)
-    console.log(`Compra em ${q[1].data} no total de R$${q[1].total.toFixed(2)}`)
-    console.log(`Total do cliente: R$${r.toFixed(2)}`)
-    console.log(`------------------------------------------------------------`)
-    console.log(`Cliente: ${s.id} - ${s.name}`)
-    console.log(`Compra em ${t[0].data} no total de R$${t[0].total.toFixed(2)}`)
-    console.log(`Compra em ${t[1].data} no total de R$${t[1].total.toFixed(2)}`)
-    console.log(`Total do cliente: R$${u.toFixed(2)}`)
-}
+// export function show(p, q, r, s, t, u) {
+//     console.log(`Cliente: ${p.id} - ${p.name}`)
+//     console.log(`Compra em ${q[0].data} no total de R$${q[0].total.toFixed(2)}`)
+//     console.log(`Compra em ${q[1].data} no total de R$${q[1].total.toFixed(2)}`)
+//     console.log(`Total do cliente: R$${r.toFixed(2)}`)
+//     console.log(`------------------------------------------------------------`)
+//     console.log(`Cliente: ${s.id} - ${s.name}`)
+//     console.log(`Compra em ${t[0].data} no total de R$${t[0].total.toFixed(2)}`)
+//     console.log(`Compra em ${t[1].data} no total de R$${t[1].total.toFixed(2)}`)
+//     console.log(`Total do cliente: R$${u.toFixed(2)}`)
+// }
